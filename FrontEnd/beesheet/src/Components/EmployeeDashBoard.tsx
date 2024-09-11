b1e13a48804d9afc9decb089a0cf942047336395
Tanishq Chaurasia <tanishq.chaurasia@beehyv.com>
2024-09-11 17:49:08 +0530

    bottom right button added

--- FrontEnd/beesheet/src/Components/EmployeeDashBoard.tsx
+++ FrontEnd/beesheet/src/Components/EmployeeDashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { jwtDecode, JwtPayload } from "jwt-decode";
 import axios from "axios";
@@ -32,14 +32,24 @@ const EmployeeDashBoard = () => {
       <div className="container-fluid">
         <div className="pt-3">
           {!taskList && <h2>No Tasks Added</h2>}
-          {taskList && taskList.map((tasks) => <div className="card p-1">
+          {taskList && taskList.map((tasks) => <div className="card p-1 mb-3">
             <div className="card-header"><div className="row justify-content-between"><span className="h5 col-7">{tasks.title}</span> <button className="btn btn-primary col-1">Edit</button></div></div>
             <div className="card-body"><p>{tasks.description}</p></div>
             <div className="card-footer"><div className="row justify-content-between"><span className="col-7">Date Added: {tasks.date} Time Spent: {tasks.time} minutes</span><button className="btn btn-danger col-1">Delete</button></div></div>
           </div>)}
         </div>
       </div>
-    </div>
+      <div className="position-fixed bottom-0 end-0 m-3">
+        <button
+          className="btn btn-primary rounded-circle d-flex align-items-center justify-content-center"
+          style={{ height: "3rem", width: "3rem" }}
+        >
+          <i
+            className="fa fa-solid fa-plus text-white"
+            style={{ fontSize: "1.5rem" }}
+          ></i>
+        </button>
+      </div>    </div>
   );
 };
 
